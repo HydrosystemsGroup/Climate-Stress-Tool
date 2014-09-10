b18c6ce42262a02bf4697ba547b93b74a3af9632
Jeff Walker <walker.jeff.d@gmail.com>
2014-09-10 18:56:57 -0400

    add annual table to weather gen

--- public/app/app.js
+++ public/app/app.js
@@ -15,17 +15,35 @@ app.service('ocpuService', ['$http', 'ocpuUrl', function($http, url) {
       });
   };
 
-  this.getMaurer = function(latitude, longitude, cb) {
-    $http.post((url + '/library/weathergen/R/get_maurer_mon'), {lat: latitude, lon: longitude}).
+  this.getMaurerMon = function(latitude, longitude, cb) {
+    $http.post((url + '/library/weathergen/R/get_maurer_mon'), 
+               {lat: +latitude, lon: +longitude}).
       success(function(data, status, headers, config) {
-        console.log('getMaurer: SUCCESS');
+        console.log('getMaurerMon: SUCCESS');
         cb(data, headers());
       }).
       error(function(data, status, headers, config) {
-        console.log('getMaurer: ERROR');
+        console.log('getMaurerMon: ERROR');
         console.log(data);
       });
   };
+
+  this.aggregateMaurerMon = function(sessionKey, cb) {
+    console.log('aggregateMaurerMon:', sessionKey);
+    $http({url: url + '/library/weathergen/R/aggregate_maurer_mon',
+           method: 'POST',
+           data: 'x='+sessionKey,
+           headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}}).
+      success(function(data, status, headers, config) {
+        console.log('aggregateMaurerMon: SUCCESS');
+        cb(data, headers());
+      }).
+      error(function(data, status, headers, config) {
+        console.log('aggregateMaurerMon: ERROR');
+        console.log(data);
+      });
+  };
+
 }]);
 
 
@@ -34,14 +52,23 @@ app.controller('IndexCtrl', ['$scope', function($scope) { }]);
 app.controller('WeatherCtrl', ['$scope', 'ocpuService', function($scope, ocpu) { 
   $scope.latitude = 42;
   $scope.longitude = -72;
+  $scope.session = null;
   $scope.getDataset = function() {
     console.log('getDataset()');
-    ocpu.getMaurer(+$scope.latitude, +$scope.longitude, function(data, headers) {
-      $scope.key = headers['x-ocpu-session'];
+    ocpu.getMaurerMon(+$scope.latitude, +$scope.longitude, function(data, headers) {
+      $scope.session = headers['x-ocpu-session'];
 
-      ocpu.getData($scope.key, 'json', function(data) {
+      ocpu.aggregateMaurerMon($scope.session, function(data, headers) {
+        var sessionAnnual = headers['x-ocpu-session'];
+        $scope.session_yr = sessionAnnual;
+
+        ocpu.getData(sessionAnnual, 'json', function(data) {
+          $scope.data_yr = data;
+        });
+      });
+
+      ocpu.getData($scope.session, 'json', function(data) {
         $scope.data = data;
-        console.log('getDataset: DONE');  
       });
     });
   };
