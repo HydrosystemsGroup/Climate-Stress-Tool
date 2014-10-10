37868e134ab7504c6549c8be336794b57e3257c3
Jeff Walker <walker.jeff.d@gmail.com>
2014-10-10 12:37:19 -0400

    add map view with ol3

--- public/app/app.js
+++ public/app/app.js
@@ -195,6 +195,70 @@ app.controller('ModelCtrl', ['$scope', 'JointService', function($scope, joint) {
   }
 }]);
 
+app.controller('MapCtrl', ['$scope', function($scope) {
+  $scope.map = {};
+
+  var feature = new ol.Feature({
+    geometry: null,
+    name: 'Null Island',
+    population: 4000,
+    rainfall: 500
+  });
+  window.feature = feature;
+
+  var vectorSource = new ol.source.Vector({
+    features: [feature]
+  });
+
+  var vectorLayer = new ol.layer.Vector({
+    source: vectorSource,
+    style: new ol.style.Style({
+      image: new ol.style.Circle({
+        radius: 5,
+        fill: new ol.style.Fill({color: '#ff0000'}),
+        stroke: new ol.style.Stroke({color: '#ffffff', width: 1})
+      })
+    })
+  });
+
+  // window.layer = layer;
+  var map = new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        // source: new ol.source.MapQuest({layer: 'sat'})
+        source: new ol.source.OSM()
+      }),
+      vectorLayer
+    ],
+    view: new ol.View({
+      center: ol.proj.transform([-72, 42], 'EPSG:4326', 'EPSG:3857'),
+      zoom: 6
+    })
+  });
+
+  map.on('click', function(evt) {
+    var coordinate = evt.coordinate;
+    var wgs84 = ol.proj.transform(coordinate, 'EPSG:3857', 'EPSG:4326');
+    // var hdms = ol.coordinate.toStringHDMS(wgs84);
+    $scope.map.coordinate = wgs84;
+    $scope.$digest();
+  });
+
+  $scope.$watch('map.coordinate', function(value) {
+    if (value) {
+      value = ol.proj.transform(value, 'EPSG:4326', 'EPSG:3857');
+      feature.setGeometry(new ol.geom.Point(value));  
+    } else {
+      feature.setGeometry();
+    }
+  });
+
+  $scope.clearCoordinate = function() {
+    delete $scope.map.coordinate;
+  };
+}])
+
 app.directive('timeseriesChart', function() {
   function link(scope, element, attr) {
     var margin = {top: 20, right: 80, bottom: 30, left: 50},
@@ -284,6 +348,10 @@ app.config(['$routeProvider',
       when('/model', {
         templateUrl: 'app/templates/model.html',
         controller: 'ModelCtrl'
+      }).
+      when('/map', {
+        templateUrl: 'app/templates/map.html',
+        controller: 'MapCtrl'
       }).
       otherwise({
         redirectTo: '/'
