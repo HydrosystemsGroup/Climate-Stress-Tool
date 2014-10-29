e46da2840b3de085e232583205dca6519fec73ca
Jeff Walker <walker.jeff.d@gmail.com>
2014-10-29 12:48:13 -0400

    add diagram controller

--- public/dist/app.js
+++ public/dist/app.js
@@ -317,6 +317,17 @@ angular.module('map')
     };
   });;
 angular.module('model')
+  .controller('DiagramCtrl', ['$scope', '$state', 'Graph', 'ModelService', function($scope, $state, graph, model) {
+    model.init($('#diagram'));
+
+    // register click event for clicking element
+    graph.onClick('cell:pointerdown', function(cellView, evt, x, y) { 
+      console.log('cell view ' + cellView.model.get('nodeType') + ' was clicked with id ' + cellView.model.get('id')); 
+      $state.go('model.node', {nodeId: cellView.model.get('id')});
+    });
+  }]);
+;
+angular.module('model')
   .controller('ModelCtrl', ['$scope', '$state', 'ModelService', 'Graph', function($scope, $state, model, graph) {
     // console.log('ModelCtrl');
 
